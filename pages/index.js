df9bbd8fe7bad1d324bfd11618493edfa94136e8
nakai <shuta.nakai@hakuhodo-ms.co.jp>
2021-11-29 19:00:08 +0900

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useEffect, useState } from 'react'
-import { useQRCode } from 'next-qrcode'
-
 import { useLiffLogin } from '../hooks/useLiffLogin'
 
 export default function Home() {
@@ -12,23 +10,13 @@ export default function Home() {
     id: '',
   };
   const [userInfo, setUserInfo] = useState(initialInfo);
-  const inputRef = useQRCode({
-    text: userInfo.id,
-    options: {
-      level: 'H',
-      margin: 2,
-      scale: 2,
-      width: 240,
-    }
-  });
   useEffect(() => {
     const getUserInfo = () => {
       liff.getProfile()
         .then((profile) => {
           const {displayName, userId} = profile;
           setUserInfo({name: displayName, id: userId});
-        })
-        .catch((error) => alert(`エラー： ${error}`));
+        }).catch((error) => alert(`エラー： ${error}`));
     };
     getUserInfo();
   }, [liff]);
@@ -39,8 +27,8 @@ export default function Home() {
     <section className="app-wrapper">
       <div className="member-card-app">
         <div className="header">
-          <p>{userInfo.name} さん</p>
-          <canvas ref={inputRef} />
+          <p>ユーザー名：{userInfo.name}</p>
+          <p>ユーザーID：{userInfo.id}</p>
         </div>
       </div>
     </section>
