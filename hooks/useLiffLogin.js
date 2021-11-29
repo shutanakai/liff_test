b479f4a32133c34d176f87dcd4c7013ff5a374ed
nakai <shuta.nakai@hakuhodo-ms.co.jp>
2021-11-29 19:39:22 +0900

    test

--- hooks/useLiffLogin.js
+++ hooks/useLiffLogin.js
@@ -4,11 +4,10 @@ export const useLiffLogin = () => {
     const initialInfo = {
         name: '',
         id: '',
-      };
-      const [userInfo, setUserInfo] = useState(initialInfo);
+    };
+    const [userInfo, setUserInfo] = useState(initialInfo);
 
     useEffect(() => {
-        let unmounted = false;
         const initLiff = async () => {
             const liff = (await import('@line/liff')).default;
             await liff.init({liffId: process.env.NEXT_PUBLIC_LIFF_ID})
@@ -26,16 +25,9 @@ export const useLiffLogin = () => {
                         alert(`エラー： ${error}`);
                     });
             };
-            if (!unmounted) {
-                setLiff(liff);
-            };
         };
-        const cleanUp = () => {
-            unmounted = true;
-        }
 
         initLiff();
-        return cleanUp;
     }, []);
 
     return userInfo;
