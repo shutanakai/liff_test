f4000ce83988b148538a6fea7301a60b21df55fa
nakai <shuta.nakai@hakuhodo-ms.co.jp>
2021-11-30 15:31:29 +0900

    QRスタイル修正

--- hooks/useLineQRCode.js
+++ hooks/useLineQRCode.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 const QRCode = require('qrcode');
 
 export const useLineQRCode = ({...props}) => {
