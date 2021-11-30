96bc23c6d0bdbbcf070f55865f8e40e9106b1857
nakai <shuta.nakai@hakuhodo-ms.co.jp>
2021-11-30 14:19:11 +0900

    QRスタイル修正

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 import { useLineQRCode } from '../hooks/useLineQRCode'
 
 export default function Home() {
-  const { inputRef } = useLineQRCode({
+  const { userInfo, inputRef } = useLineQRCode({
     options: {
       level: 'H',
       margin: 2,
@@ -11,15 +11,14 @@ export default function Home() {
       width: 240,
     },
   });
-  console.log(inputRef);
 
   return (
     <>
     <section className="app-wrapper">
       <div className="member-card-app">
         <div className="header">
-          <p>ユーザー名：{inputRef.current ? inputRef.current.userInfo.name : ""}</p>
-          <p>ユーザーID：{inputRef.current ? inputRef.current.userInfo.id : ""}</p>
+          <p>ユーザー名：{userInfo.name}</p>
+          <p>ユーザーID：{userInfo.id}</p>
           <img
             ref={inputRef}
           />
