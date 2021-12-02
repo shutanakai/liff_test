dc27ec0530a1c4dcba7e341b9cf63b103153423c
nakai <shuta.nakai@hakuhodo-ms.co.jp>
2021-12-02 17:17:14 +0900

    flex messageの追加 #3

--- utils/flexMessage.js
+++ utils/flexMessage.js
@@ -1,156 +1,244 @@
 export const flexMessages = [
     {
-        type: 'text',
-        text: 'text',
+        type: 'carousel',
+        contents: [
+            {
+                type: 'bubble',
+                size: 'nano',
+                header: {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                        {
+                            type: 'text',
+                            text: 'In Progress',
+                            color: '#ffffff',
+                            align: 'start',
+                            size: 'md',
+                            gravity: 'center',
+                        },
+                        {
+                            type: 'text',
+                            text: '70%',
+                            color: '#ffffff',
+                            align: 'start',
+                            size: 'xs',
+                            gravity: 'center',
+                            margin: 'lg',
+                        },
+                        {
+                            type: 'box',
+                            layout: 'vertical',
+                            contents: [
+                                {
+                                    type: 'box',
+                                    layout: 'vertical',
+                                    contents: [
+                                        {
+                                            type: 'filler',
+                                        },
+                                    ],
+                                    width: '70%',
+                                    backgroundColor: '#0D8186',
+                                    height: '6px',
+                                },
+                            ],
+                            backgroundColor: '#9FD8E36E',
+                            height: '6px',
+                            margin: 'sm',
+                        },
+                    ],
+                    backgroundColor: '#27ACB2',
+                    paddingTop: '19px',
+                    paddingAll: '12px',
+                    paddingBottom: '16px',
+                },
+                body: {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                        {
+                            type: 'box',
+                            layout: 'horizontal',
+                            contents: [
+                                {
+                                    type: 'text',
+                                    text: 'Buy milk and lettuce before class',
+                                    color: '#8C8C8C',
+                                    size: 'sm',
+                                    wrap: true,
+                                },
+                            ],
+                            flex: 1,
+                        },
+                    ],
+                    spacing: 'md',
+                    paddingAll: '12px',
+                },
+                styles: {
+                    footer: {
+                        separator: false,
+                    },
+                },
+            },
+            {
+                type: 'bubble',
+                size: 'nano',
+                header: {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                        {
+                            type: 'text',
+                            text: 'Pending',
+                            color: '#ffffff',
+                            align: 'start',
+                            size: 'md',
+                            gravity: 'center',
+                        },
+                        {
+                            type: 'text',
+                            text: '30%',
+                            color: '#ffffff',
+                            align: 'start',
+                            size: 'xs',
+                            gravity: 'center',
+                            margin: 'lg',
+                        },
+                        {
+                            type: 'box',
+                            layout: 'vertical',
+                            contents: [
+                                {
+                                    type: 'box',
+                                    layout: 'vertical',
+                                    contents: [
+                                        {
+                                            type: 'filler',
+                                        },
+                                    ],
+                                    width: '30%',
+                                    backgroundColor: '#DE5658',
+                                    height: '6px',
+                                },
+                            ],
+                            backgroundColor: '#FAD2A76E',
+                            height: '6px',
+                            margin: 'sm',
+                        },
+                    ],
+                    backgroundColor: '#FF6B6E',
+                    paddingTop: '19px',
+                    paddingAll: '12px',
+                    paddingBottom: '16px',
+                },
+                body: {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                        {
+                            type: 'box',
+                            layout: 'horizontal',
+                            contents: [
+                                {
+                                    type: 'text',
+                                    text: 'Wash my car',
+                                    color: '#8C8C8C',
+                                    size: 'sm',
+                                    wrap: true,
+                                },
+                            ],
+                            flex: 1,
+                        },
+                    ],
+                    spacing: 'md',
+                    paddingAll: '12px',
+                },
+                styles: {
+                    footer: {
+                        separator: false,
+                    },
+                },
+            },
+            {
+                type: 'bubble',
+                size: 'nano',
+                header: {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                        {
+                            type: 'text',
+                            text: 'In Progress',
+                            color: '#ffffff',
+                            align: 'start',
+                            size: 'md',
+                            gravity: 'center',
+                        },
+                        {
+                            type: 'text',
+                            text: '100%',
+                            color: '#ffffff',
+                            align: 'start',
+                            size: 'xs',
+                            gravity: 'center',
+                            margin: 'lg',
+                        },
+                        {
+                            type: 'box',
+                            layout: 'vertical',
+                            contents: [
+                                {
+                                    type: 'box',
+                                    layout: 'vertical',
+                                    contents: [
+                                        {
+                                            type: 'filler',
+                                        },
+                                    ],
+                                    width: '100%',
+                                    backgroundColor: '#7D51E4',
+                                    height: '6px',
+                                },
+                            ],
+                            backgroundColor: '#9FD8E36E',
+                            height: '6px',
+                            margin: 'sm',
+                        },
+                    ],
+                    backgroundColor: '#A17DF5',
+                    paddingTop: '19px',
+                    paddingAll: '12px',
+                    paddingBottom: '16px',
+                },
+                body: {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                        {
+                            type: 'box',
+                            layout: 'horizontal',
+                            contents: [
+                                {
+                                    type: 'text',
+                                    text: 'Buy milk and lettuce before class',
+                                    color: '#8C8C8C',
+                                    size: 'sm',
+                                    wrap: true,
+                                },
+                            ],
+                            flex: 1,
+                        },
+                    ],
+                    spacing: 'md',
+                    paddingAll: '12px',
+                },
+                styles: {
+                    footer: {
+                        separator: false,
+                    },
+                },
+            },
+        ],
     },
-    // {
-    //     type: 'bubble',
-    //     hero: {
-    //         type: 'image',
-    //         url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
-    //         size: 'full',
-    //         aspectRatio: '20:13',
-    //         aspectMode: 'cover',
-    //         action: {
-    //             type: 'uri',
-    //             uri: 'http://linecorp.com/',
-    //         },
-    //     },
-    //     body: {
-    //         type: 'box',
-    //         layout: 'vertical',
-    //         contents: [
-    //             {
-    //                 type: 'text',
-    //                 text: 'Brown Cafe',
-    //                 weight: 'bold',
-    //                 size: 'xl',
-    //             },
-    //             {
-    //                 type: 'box',
-    //                 layout: 'baseline',
-    //                 margin: 'md',
-    //                 contents: [
-    //                     {
-    //                         type: 'icon',
-    //                         size: 'sm',
-    //                         url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
-    //                     },
-    //                     {
-    //                         type: 'icon',
-    //                         size: 'sm',
-    //                         url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
-    //                     },
-    //                     {
-    //                         type: 'icon',
-    //                         size: 'sm',
-    //                         url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
-    //                     },
-    //                     {
-    //                         type: 'icon',
-    //                         size: 'sm',
-    //                         url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
-    //                     },
-    //                     {
-    //                         type: 'icon',
-    //                         size: 'sm',
-    //                         url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png',
-    //                     },
-    //                     {
-    //                         type: 'text',
-    //                         text: '4.0',
-    //                         size: 'sm',
-    //                         color: '#999999',
-    //                         margin: 'md',
-    //                         flex: 0,
-    //                     },
-    //                 ],
-    //             },
-    //             {
-    //                 type: 'box',
-    //                 layout: 'vertical',
-    //                 margin: 'lg',
-    //                 spacing: 'sm',
-    //                 contents: [
-    //                     {
-    //                         type: 'box',
-    //                         layout: 'baseline',
-    //                         spacing: 'sm',
-    //                         contents: [
-    //                             {
-    //                                 type: 'text',
-    //                                 text: 'Place',
-    //                                 color: '#aaaaaa',
-    //                                 size: 'sm',
-    //                                 flex: 1,
-    //                             },
-    //                             {
-    //                                 type: 'text',
-    //                                 text: 'Miraina Tower, 4-1-6 Shinjuku, Tokyo',
-    //                                 wrap: true,
-    //                                 color: '#666666',
-    //                                 size: 'sm',
-    //                                 flex: 5,
-    //                             },
-    //                         ],
-    //                     },
-    //                     {
-    //                         type: 'box',
-    //                         layout: 'baseline',
-    //                         spacing: 'sm',
-    //                         contents: [
-    //                             {
-    //                                 type: 'text',
-    //                                 text: 'Time',
-    //                                 color: '#aaaaaa',
-    //                                 size: 'sm',
-    //                                 flex: 1,
-    //                             },
-    //                             {
-    //                                 type: 'text',
-    //                                 text: '10:00 - 23:00',
-    //                                 wrap: true,
-    //                                 color: '#666666',
-    //                                 size: 'sm',
-    //                                 flex: 5,
-    //                             },
-    //                         ],
-    //                     },
-    //                 ],
-    //             },
-    //         ],
-    //     },
-    //     footer: {
-    //         type: 'box',
-    //         layout: 'vertical',
-    //         spacing: 'sm',
-    //         contents: [
-    //             {
-    //                 type: 'button',
-    //                 style: 'link',
-    //                 height: 'sm',
-    //                 action: {
-    //                     type: 'uri',
-    //                     label: 'CALL',
-    //                     uri: 'https://linecorp.com',
-    //                 },
-    //             },
-    //             {
-    //                 type: 'button',
-    //                 style: 'link',
-    //                 height: 'sm',
-    //                 action: {
-    //                     type: 'uri',
-    //                     label: 'WEBSITE',
-    //                     uri: 'https://linecorp.com',
-    //                 },
-    //             },
-    //             {
-    //                 type: 'spacer',
-    //                 size: 'sm',
-    //             },
-    //         ],
-    //         flex: 0,
-    //     },
-    // },
 ];
