ec10f32d55a797890d02639ea91a104a1a8df118
nakai <shuta.nakai@hakuhodo-ms.co.jp>
2021-12-02 16:51:58 +0900

    sendMessagesの追加 #24

--- pages/index.js
+++ pages/index.js
@@ -5,64 +5,63 @@ import QRCode from "qrcode.react"
 
 
 export default memo(function Home() {
-  const [token, setToken] = useState(null);
-  const [err, setErr] = useState("");
-  const message = [{
-    type: 'text',
-    text: 'Hello, World!'
-  }];
+    const [token, setToken] = useState(null);
+    const message = [{
+        type: 'text',
+        text: 'Hello, World!'
+    }];
 
-  useEffect(
-      () => {
-          const initLiff = async () => {
-              const liff = (await import('@line/liff')).default;
-              await liff.init({liffId: process.env.NEXT_PUBLIC_LIFF_ID})
-                  .then(() => {
-                      const idToken = liff.getDecodedIDToken();
-                      setToken(idToken);
-                  })
-                  .catch((err) => {
-                      alert(`LIFFの初期化失敗。\n${err}`);
-              });
-              if (!liff.isLoggedIn()) {
-                  liff.login();
-              };
-              return liff;
-          };
+    useEffect(
+        () => {
+            const initLiff = async () => {
+                const liff = (await import('@line/liff')).default;
+                await liff.init({liffId: process.env.NEXT_PUBLIC_LIFF_ID})
+                    .then(() => {
+                        const idToken = liff.getDecodedIDToken();
+                        setToken(idToken);
+                    })
+                    .catch((err) => {
+                        alert(`LIFFの初期化失敗。\n${err}`);
+                });
+                if (!liff.isLoggedIn()) {
+                    liff.login();
+                };
+                return liff;
+            };
 
-          initLiff();
-      },
-      [setToken],
-  );
+            initLiff();
+        },
+        [setToken],
+    );
 
-  const sendMessages = async (messages) => {
-    const liff = (await import('@line/liff')).default;
-      await liff.init({liffId: process.env.NEXT_PUBLIC_LIFF_ID})
-          .then(() => {
-            liff.sendMessages(messages);
-            liff.closeWindow();
-          })
-          .catch((err) => {
-            alert(`メッセージ送信失敗\n${err}`);
-      });
-  }
+    const sendMessages = async (messages) => {
+        const liff = (await import('@line/liff')).default;
+        await liff.init({liffId: process.env.NEXT_PUBLIC_LIFF_ID})
+            .then(() => {
+                liff.sendMessages(messages);
+                liff.closeWindow();
+            })
+            .catch((err) => {
+                alert(`メッセージ送信失敗\n${err}`);
+        });
+    }
 
-  return (
-    <>
-    <section className="app-wrapper">
-      <div className="member-card-app">
-        <div>
-          <p>ユーザー名：{token ? token.name : "取得できませんでした"}</p>
-          <p>ユーザーID：{token ? token.sub : "取得できませんでした"}</p>
-          {token ? (
-            <QRCode value={token.sub} />
-          ) : (
-            <p>tokenがありません</p>
-          )}
-          <button onClick={() => sendMessages(message)}>メッセージ送信</button>
+    return (
+        <>
+        <section className="app-wrapper">
+        <div className="member-card-app">
+            <div>
+            <p>ユーザー名：{token ? token.name : "取得できませんでした"}</p>
+            <p>ユーザーID：{token ? token.sub : "取得できませんでした"}</p>
+            {token ? (
+                <QRCode value={token.sub} />
+            ) : (
+                <p>tokenがありません</p>
+            )}
+            <button onClick={() => sendMessages(message)}>メッセージ送信</button>
+            </div>
         </div>
-      </div>
-    </section>
-    </>
-  )
+        </section>
+        </>
+    )
 });
